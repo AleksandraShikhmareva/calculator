81d03bc0d18097f2488206b2605b3319f0d2ad6a
AleksandraShikhmareva <aleksandrashih@gmail.com>
2022-05-22 23:22:49 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,5 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.sumTwoNumbers();
-        toFailBuild
     }
-
 }
